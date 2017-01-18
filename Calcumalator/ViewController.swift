ee1e1e89486ced7eb2d7dcff58b54baea71d35e6
Jesse Baril <jessebaril.programmer@gmail.com>
2017-01-18 10:27:30 -0500

    Initial Commit

---
+++ Calcumalator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calcumalator
+//
+//  Created by Jesse Baril on 2017-01-18.
+//  Copyright © 2017 Jesse Baril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
